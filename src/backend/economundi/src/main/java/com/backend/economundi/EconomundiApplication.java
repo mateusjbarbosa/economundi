f40948580dedb386cd0c686ae10d2c6823d5b6c0
João Vitor Teixeira <joaovitorteixeira10.jvt@gmail.com>
2019-05-22 17:57:45 -0300

    Commit inicial do backend

---
+++ src/backend/economundi/src/main/java/com/backend/economundi/EconomundiApplication.java
@@ -0,0 +1,13 @@
+package com.backend.economundi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomundiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomundiApplication.class, args);
+	}
+
+}
