4eee829fda600162d9c41bf20b5537c9e2c94430
João Vitor Teixeira <joaovitorteixeira10.jvt@gmail.com>
2019-05-22 18:59:54 -0300

    Implementação do Scheduled do Spring

--- src/backend/economundi/src/main/java/com/backend/economundi/EconomundiApplication.java
+++ src/backend/economundi/src/main/java/com/backend/economundi/EconomundiApplication.java
@@ -1,13 +1,31 @@
 package com.backend.economundi;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@Component
+@EnableScheduling
 public class EconomundiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EconomundiApplication.class, args);
-	}
+    private final long SEGUNDO = 1000;
+    private final long MINUTO = SEGUNDO * 60;
+    private final long HORA = MINUTO * 60;
 
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    
+    public static void main(String[] args) {
+        SpringApplication.run(EconomundiApplication.class, args);
+
+    }
+
+    @Scheduled(fixedDelay = MINUTO)
+    public void reportCurrentTime() {
+        System.out.println("The time is now " + dateFormat.format(new Date()));
+    }
 }
