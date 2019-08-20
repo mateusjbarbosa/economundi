ce554fd5bff37c2a6ff616758d71f98f03803f52
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2019-08-20 17:54:41 -0300

    Adição do "Fake Menu"

--- src/frontend/economundi/src/App.js
+++ src/frontend/economundi/src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import MenuBar from "./components/MenuBar/MenuBar";
+
+import "./app.scss";
+
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <MenuBar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/frontend/economundi/src/components/MenuBar/MenuBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./menu-bar.scss";
+
+function MenuBar() {
+  return (
+    <section class="menu-container">
+      <div class="menu-item">PE</div>
+      <div class="menu-item">N</div>
+      <div class="menu-item">I</div>
+      <div class="menu-item">S</div>
+      <div class="menu-item">D</div>
+      <div class="menu-item">SN</div>
+      <div class="menu-item">P</div>
+    </section>
+  );
+}
+
+export default MenuBar;
