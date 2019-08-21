12c670698ba2c6b32a2b14fda3af55b24b21c4b8
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2019-08-21 18:22:00 -0300

    Primeira conexão com a API do EconoMundi

--- src/frontend/economundi/src/containers/Dicionario/Dicionario.js
+++ src/frontend/economundi/src/containers/Dicionario/Dicionario.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Dicionario() {
-  return <h1>Dicionario</h1>;
+import api from "../../services/api";
+
+import "./dicionario.scss";
+
+class Dicionario extends Component {
+  state = {
+    searchedWord: {}
+  };
+  componentDidMount() {
+    this.getWord();
+  }
+
+  getWord = async () => {
+    const response = await api.get("api/v1/palavra/10");
+
+    console.log(response);
+    this.setState({ searchedWord: response.data });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Dicionario</h1>
+        <h2>{this.state.searchedWord.name}</h2>
+        <div className="desc-box">
+          <p>{this.state.searchedWord.description}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Dicionario;
