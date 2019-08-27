69ace03cfc8f1e70d2e9b292fa6fc207c06b265b
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2019-08-27 19:14:18 -0300

    Agora sim, voltamos. Segue o jogo

--- src/frontend/economundi/src/containers/Dicionario/Dicionario.js
+++ src/frontend/economundi/src/containers/Dicionario/Dicionario.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Dicionario() {
-  return <h1>Dicionário</h1>;
+import api from "../../services/api";
+
+class Dicionario extends Component {
+  state = {
+    searchedWord: {}
+  };
+
+  componentDidMount() {
+    this.getWord();
+  }
+
+  getWord = async () => {
+    const response = await api.get("api/v1/palavra/10");
+
+    this.setState({ searchedWord: response.data });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Dicionário</h1>
+        <div>
+          <p>{this.state.searchedWord.description}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Dicionario;
