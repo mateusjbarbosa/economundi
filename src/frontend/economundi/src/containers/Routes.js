8871a097684b8ad0dcedf3712b1a3ccd1b3bbb81
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2019-08-29 17:47:56 -0300

    Criação do arquivo variables.scss para colocar as cores, inicialmente

--- src/frontend/economundi/src/containers/Routes.js
+++ src/frontend/economundi/src/containers/Routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
-import { Dicionario } from "../containers";
+import { Dictionary } from "../containers";
 
 const Routes = () => (
   <Switch>
