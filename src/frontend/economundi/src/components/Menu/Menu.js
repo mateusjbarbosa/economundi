5035dfabce587546e8a7f018c47b46908d08c89b
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2019-08-30 18:30:15 -0300

    Preparação para inserção dos ícones do menu

--- src/frontend/economundi/src/components/Menu/Menu.js
+++ src/frontend/economundi/src/components/Menu/Menu.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { ReactComponent as IconeLogo } from "../../img/IconeLogo.svg";
+import { ReactComponent as IconeNoticia } from "../../img/IconeNoticia.svg";
+import { ReactComponent as IconeIndices } from "../../img/IconeIndice.svg";
+import { ReactComponent as IconeSimulacoes } from "../../img/IconeSimulacao.svg";
+import { ReactComponent as IconeDicionario } from "../../img/IconeDicionario.svg";
+import { ReactComponent as IconeSobre } from "../../img/IconeSobre.svg";
+import { ReactComponent as IconePerfil } from "../../img/IconePerfil.svg";
+
 import "./menu.scss";
 
 function Menu() {
@@ -8,25 +16,39 @@ function Menu() {
     <nav class="col-1 econo-menu">
       <ul class="econo-menu-list">
         <li class="econo-menu-item">
-          <Link to="/">PE</Link>
+          <Link to="/">
+            <IconeLogo width="60" height="60" />
+          </Link>
         </li>
         <li class="econo-menu-item">
-          <Link to="/">N</Link>
+          <Link to="/">
+            <IconeNoticia width="60" height="60" />
+          </Link>
         </li>
         <li class="econo-menu-item">
-          <Link to="/">I</Link>
+          <Link to="/">
+            <IconeIndices width="60" height="60" />
+          </Link>
         </li>
         <li class="econo-menu-item">
-          <Link to="/">S</Link>
+          <Link to="/">
+            <IconeSimulacoes width="60" height="60" />
+          </Link>
         </li>
         <li class="econo-menu-item">
-          <Link to="/dicionario">D</Link>
+          <Link to="/dicionario">
+            <IconeDicionario width="60" height="60" />
+          </Link>
         </li>
         <li class="econo-menu-item">
-          <Link to="/">SN</Link>
+          <Link to="/">
+            <IconeSobre width="60" height="60" />
+          </Link>
         </li>
         <li class="econo-menu-item">
-          <Link to="/">P</Link>
+          <Link to="/">
+            <IconePerfil width="60" height="60" />
+          </Link>
         </li>
       </ul>
     </nav>
