f1010cbd7e9d3c83a202bb6f5a83dcd4b4b2810a
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2019-09-21 16:18:08 -0300

    Responsividade das notícias

--- src/frontend/economundi/src/components/BoxNews/BoxNews.js
+++ src/frontend/economundi/src/components/BoxNews/BoxNews.js
@@ -3,10 +3,10 @@ import React from "react";
 import "./boxNews.scss";
 
 const BoxNews = ({ news }) => {
-  console.log(news);
   return (
     <div className="box-news">
       <img src={news.titleToImage} alt={news.description} />
+      <span />
       <div className="box-news-title">
         <h2>{news.title}</h2>
       </div>
