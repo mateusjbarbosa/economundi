54224fe24f1f588a3ad46f3760d0dbc818787a19
João Vitor Teixeira <joaovitorteixeira10.jvt@gmail.com>
2019-09-23 22:58:29 -0300

    Melhorias no código

--- src/backend/economundi-api/src/main/java/com/backend/economundi/consumer/ApiNewsConsumer.java
+++ src/backend/economundi-api/src/main/java/com/backend/economundi/consumer/ApiNewsConsumer.java
@@ -5,21 +5,31 @@ import com.backend.economundi.service.NewsService;
 import java.io.IOException;
 
 public class ApiNewsConsumer {
-	
-	private final String URL_BR = "https://newsapi.org/v2/top-headlines?country=br&category="
-			+ "business&apiKey=e8f44036f3254db8bcd6e31c3c9ca1b0";
 
-	public void refreshNews() throws IOException {
-		ApiConsumerGeneric generic = new ApiConsumerGeneric();
-		ResultNews result = generic.getData(URL_BR, ResultNews.class);
-		NewsService service = new NewsService();
+    private final String URL_BR = "https://newsapi.org/v2/top-headlines?country=br&category="
+            + "business&apiKey=e8f44036f3254db8bcd6e31c3c9ca1b0";
+    private final String URL_WORLD = "https://newsapi.org/v2/top-headlines?category="
+            + "business&country=us&apiKey=e8f44036f3254db8bcd6e31c3c9ca1b0";
 
-		result.getArticles().forEach((news) -> {
-			news.setLocality("Brazil");
+    public void refreshNews() throws IOException {
+        ApiConsumerGeneric generic = new ApiConsumerGeneric();
+        NewsService service = new NewsService();
+        ResultNews resultBr = generic.getData(URL_BR, ResultNews.class);
+        ResultNews resultWorld = generic.getData(URL_WORLD, ResultNews.class);
 
-			service.create(news);
-		});
+        createNews("Brazil", resultBr);
+        createNews("World", resultWorld);
 
-		service.updateAllNewsWithRelevance();
-	}
+        service.updateAllNewsWithRelevance();
+    }
+
+    private void createNews(String locality, ResultNews resultNews) {
+        NewsService service = new NewsService();
+        
+        resultNews.getArticles().forEach((news) -> {
+            news.setLocality(locality);
+
+            service.create(news);
+        });
+    }
 }
