efb5cfd0b565311a2e3f6e26cf6db03f3c50ce1e
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2019-10-03 15:36:36 -0300

    Alteração na API para conseguir comunicar com os métodos do backend

--- src/frontend/economundi/src/services/api.js
+++ src/frontend/economundi/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8082/economundi/api/v1"
+  baseURL: "http://localhost:8082/economundi/"
 });
 
 export default api;
