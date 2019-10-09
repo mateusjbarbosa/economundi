2aad984971ba67c0ed6afb2e21fc2ff95115f8ec
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2019-10-09 18:09:20 -0300

    Tratamento de erros do Login e algumas correções no Registro

--- src/frontend/economundi/src/containers/Login/Login.js
+++ src/frontend/economundi/src/containers/Login/Login.js
@@ -10,7 +10,12 @@ import "./login.scss";
 class Login extends Component {
   constructor(props) {
     super(props);
-    this.state = { email: "", password: "" };
+    this.state = {
+      email: "",
+      password: "",
+      error_login: false,
+      statusLogin: true
+    };
   }
 
   onEmailChanged = async e => {
@@ -25,48 +30,111 @@ class Login extends Component {
     this.setState(() => ({ password: value }));
   };
 
+  validate = async () => {
+    const { email, password } = this.state;
+
+    let error = false;
+
+    this.setState({ error_login: false });
+
+    if (email === "" || password === "") {
+      error = true;
+
+      this.setState({ error_login: true });
+    }
+
+    return error;
+  };
+
   onLoginSubmit = async e => {
-    const response = await api.post("/login", {
-      email: this.state.email,
-      password: this.state.password
-    });
+    const errors = await this.validate();
 
-    localStorage.setItem("tokenUser", response.data);
+    if (!errors) {
+      const response = await api.post("/login", {
+        email: this.state.email,
+        password: this.state.password
+      });
 
-    window.history.back();
+      console.log(response);
+
+      if (response.status === 200) {
+        localStorage.setItem("tokenUser", response.data);
+
+        window.history.back();
+      } else {
+        this.setState({ statusLogin: false });
+      }
+    }
   };
 
   render() {
+    const { error_login, statusLogin } = this.state;
+
     return (
       <>
         <div className="login-title">
           <h1>Login</h1>
         </div>
 
-        <div className="login">
-          <h2>E-mail:</h2>
-          <input
-            type="email"
-            onChange={this.onEmailChanged}
-            placeholder="Qual o seu e-mail?"
-          />
+        {error_login ? (
+          <div className="login-error">
+            <h2>E-mail:</h2>
+            <input
+              type="email"
+              onChange={this.onEmailChanged}
+              placeholder="Qual o seu e-mail?"
+            />
 
-          <h2>Senha:</h2>
-          <input
-            type="password"
-            onChange={this.onPassChanged}
-            placeholder="Insira uma senha!"
-          />
+            <h2>Senha:</h2>
+            <input
+              type="password"
+              onChange={this.onPassChanged}
+              placeholder="Insira uma senha!"
+            />
 
-          <button type="submit" onClick={this.onLoginSubmit}>
-            Logar
-          </button>
+            <button type="submit" onClick={this.onLoginSubmit}>
+              Logar
+            </button>
 
-          <div className="login-social">
-            <img src={FacebookIcon} alt="Facebook" />
-            <img src={GoogleIcon} alt="Google" />
+            <div className="login-social">
+              <img src={FacebookIcon} alt="Facebook" />
+              <img src={GoogleIcon} alt="Google" />
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="login">
+            <h2>E-mail:</h2>
+            <input
+              type="email"
+              onChange={this.onEmailChanged}
+              placeholder="Qual o seu e-mail?"
+            />
+
+            <h2>Senha:</h2>
+            <input
+              type="password"
+              onChange={this.onPassChanged}
+              placeholder="Insira uma senha!"
+            />
+
+            <button type="submit" onClick={this.onLoginSubmit}>
+              Logar
+            </button>
+
+            <div className="login-social">
+              <img src={FacebookIcon} alt="Facebook" />
+              <img src={GoogleIcon} alt="Google" />
+            </div>
+          </div>
+        )}
+
+        {!statusLogin ? (
+          <div className="pop-up-red">
+            <h2>Login não encontrado, tente novamente!</h2>
+          </div>
+        ) : (
+          <div></div>
+        )}
 
         <a href="/login" className="link-reminder">
           Esqueceu a senha? Relaxa, recupere-a aqui!
