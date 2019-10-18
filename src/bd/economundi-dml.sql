7d93d25967d48c927b73570ce107cdd9c5b33c44
João Vitor Teixeira <joaovitorteixeira10.jvt@gmail.com>
2019-10-17 21:39:16 -0300

    Melhoria no DML

--- src/bd/economundi-dml.sql
+++ src/bd/economundi-dml.sql
@@ -6,6 +6,7 @@ INSERT INTO news_black_list (name, id) VALUES ('LOTERIA FEDERAL', nextval('news_
 INSERT INTO news_black_list (name, id) VALUES ('MEGA-SENA', nextval('news_black_list_id_seq'));
 INSERT INTO news_black_list (name, id) VALUES ('QUINA', nextval('news_black_list_id_seq'));
 INSERT INTO news_black_list (name, id) VALUES ('LOTOFÁCIL', nextval('news_black_list_id_seq'));
+INSERT INTO news_black_list (name, id) VALUES ('DUPLA SENA', nextval('news_black_list_id_seq'));
 
 -- Inserindo palavras no dicionário.
 INSERT INTO word (name, description) VALUES ('AÇÃO COLETIVA', 'Ação intencional, de um ou vários grupos, que tem por objetivo intervir na área econômica, política, ou social.');
