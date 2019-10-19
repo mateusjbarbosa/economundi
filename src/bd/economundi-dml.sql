e260c3912a26d45bd9cde2fccf06bb53cd5dfa8f
João Vitor Teixeira <joaovitorteixeira10.jvt@gmail.com>
2019-10-19 17:37:27 -0300

    Melhoria no DML

--- src/bd/economundi-dml.sql
+++ src/bd/economundi-dml.sql
@@ -1,12 +1,12 @@
 BEGIN;
 
 -- Inserindo black list.
-INSERT INTO news_black_list (name, id) VALUES ('CONCURSO', nextval('news_black_list_id_seq'));
-INSERT INTO news_black_list (name, id) VALUES ('LOTERIA FEDERAL', nextval('news_black_list_id_seq'));
-INSERT INTO news_black_list (name, id) VALUES ('MEGA-SENA', nextval('news_black_list_id_seq'));
-INSERT INTO news_black_list (name, id) VALUES ('QUINA', nextval('news_black_list_id_seq'));
-INSERT INTO news_black_list (name, id) VALUES ('LOTOFÁCIL', nextval('news_black_list_id_seq'));
-INSERT INTO news_black_list (name, id) VALUES ('DUPLA SENA', nextval('news_black_list_id_seq'));
+INSERT INTO news_black_list (name) VALUES ('CONCURSO');
+INSERT INTO news_black_list (name) VALUES ('LOTERIA FEDERAL');
+INSERT INTO news_black_list (name) VALUES ('MEGA-SENA');
+INSERT INTO news_black_list (name) VALUES ('QUINA');
+INSERT INTO news_black_list (name) VALUES ('LOTOFÁCIL');
+INSERT INTO news_black_list (name) VALUES ('DUPLA SENA');
 
 -- Inserindo ações.
 INSERT INTO stocks (symbol, name) VALUES ('BIDI4', 'Banco Inter S.A.');
