5324e5f21733768f715f78e625a676aa139392e0
João Vitor Teixeira <joaovitorteixeira10.jvt@gmail.com>
2019-10-20 13:06:20 -0300

    Início da implementação de like nas notícias

--- src/backend/economundi-api/src/main/java/com/backend/economundi/controller/NewsController.java
+++ src/backend/economundi-api/src/main/java/com/backend/economundi/controller/NewsController.java
@@ -2,15 +2,18 @@ package com.backend.economundi.controller;
 
 import com.backend.economundi.database.dao.entity.NewsEntity;
 import com.backend.economundi.service.NewsService;
+import com.backend.economundi.service.ValidateUserService;
 
 import java.util.HashMap;
 import java.util.Map;   
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
@@ -18,7 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class NewsController {
 
     private final String PATH_URL = "/api/v1/news/";
-    private final NewsService service = new NewsService();
+    private final NewsService serviceNews = new NewsService();
+    private final ValidateUserService serviceUser = new ValidateUserService();
 
     /**
      * Retorna as notícias de acordo com a página indicada.
@@ -30,7 +34,7 @@ public class NewsController {
     @GetMapping(PATH_URL + "{locality}/" + "{page}")
     public ResponseEntity<?> getNewsByPage(@PathVariable("locality") String locality,
             @PathVariable("page") Long page) {
-        return new ResponseEntity<>(service.readByPage(page, locality),
+        return new ResponseEntity<>(serviceNews.readByPage(page, locality),
                 null, HttpStatus.ACCEPTED);
     }
 
@@ -42,7 +46,7 @@ public class NewsController {
      */
     @GetMapping(PATH_URL + "{id}")
     public ResponseEntity<?> getNews(@PathVariable("id") Long id) {
-        NewsEntity news = service.readById(id);
+        NewsEntity news = serviceNews.readById(id);
 
         if (news != null) {
             return new ResponseEntity<>(news, null, HttpStatus.ACCEPTED);
@@ -53,5 +57,14 @@ public class NewsController {
 
             return new ResponseEntity<>(error, null, HttpStatus.ACCEPTED);
         }
+    }
+    
+    @PostMapping(PATH_URL + "/protected/{id}/" + "{typeLike}")
+    @Secured({"ROLE_ADMIN", "ROLE_USER"})
+    public ResponseEntity<?> likeNews(@PathVariable("id") Long id,
+            @PathVariable("typeLike") String typeLike) {
+        String user = serviceUser.verifyUserLogin();
+        
+        return new ResponseEntity<>(user, null, HttpStatus.ACCEPTED);
     }
 }
